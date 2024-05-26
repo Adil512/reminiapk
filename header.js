00690575441453b05ee6bfbac9e835162b52d9d9
Muhammad Adil <alijee2929@gmail.com>
2024-05-26 16:02:57 +0400

    Update header.js

--- header.js
+++ header.js
@@ -1,1 +1,38 @@
 
+// components/Header.js
+
+import Link from 'next/link';
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href="/">
+          <a>MyLogo</a>
+        </Link>
+      </div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
